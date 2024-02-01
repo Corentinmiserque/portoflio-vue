7d36944d3d68a3197cc80f99409f9201a4a5f724
Corentinmiserque <corentinmiserque1@gmail.com>
2024-02-01 20:02:14 +0100

    img

--- src/stores/projects.js
+++ src/stores/projects.js
@@ -18,7 +18,7 @@ export const useProjectsStore = defineStore({
         title: 'IMEP',
         text: 'work in progress.',
         tag: ['Twig', 'Scss', 'Team'], 
-        picture: { url: "/public/IMEP.png", alt: 'IMEP' },
+        picture: { url: "/IMEP.png", alt: 'IMEP' },
         url: 'https://www.imep.be/',
       },
 
@@ -26,7 +26,7 @@ export const useProjectsStore = defineStore({
         title: 'Pendu',
         text: 'work in progress.',
         tag: ['Javascript','Solo'], 
-        picture: { url: "/public/Pendu.png", alt: 'Pendu' },
+        picture: { url: "Pendu.png", alt: 'Pendu' },
         url: 'https://xf383y.csb.app/',
         codesandbox: 'https://codesandbox.io/p/sandbox/ex2-starter-forked-xf383y?file=%2Fdraw.io.diagram.drawio.xml',
       },
@@ -35,7 +35,7 @@ export const useProjectsStore = defineStore({
         title: 'Food Matcher',
         text: 'work in progress.',
         tag: [ 'Javascript', 'Solo'], 
-        picture: { url: "/public/Fruits-Matcher.png", alt: 'Fruits Matcher' },
+        picture: { url: "/Fruits-Matcher.png", alt: 'Fruits Matcher' },
         url: 'https://gljqp7.csb.app/',
         codesandbox:"https://codesandbox.io/p/sandbox/ex4-starter-forked-gljqp7?file=%2Fsrc%2Findex.js"
       },
@@ -44,7 +44,7 @@ export const useProjectsStore = defineStore({
         title: 'COGIP',
         text: 'work in progress.',
         tag: ['React', 'Scss', 'Team'], 
-        picture: { url: "/public/COGIP.png", alt: 'COGIP' },
+        picture: { url: "/COGIP.png", alt: 'COGIP' },
         url: 'https://cogip-project-team2.000webhostapp.com//',
         github:"https://github.com/Corentinmiserque/Cogip_Project"
       },
@@ -52,7 +52,7 @@ export const useProjectsStore = defineStore({
         title: 'Az-Store',
         text: 'work in progress.',
         tag: ['Php', 'Scss', "Html" ,'Team'], 
-        picture: { url: "/public/Az-Store.png", alt: 'Az-Store' },
+        picture: { url: "/Az-Store.png", alt: 'Az-Store' },
         url: 'https://miserque-corentin.com/',
         github:"https://github.com/Corentinmiserque/AZ-Store"
       },
@@ -60,7 +60,7 @@ export const useProjectsStore = defineStore({
         title: 'Dark Kitchen',
         text: 'work in progress.',
         tag: ['Javascript', 'Html', 'Scss', 'Team'], 
-        picture: { url: "/public/Dark-Kitchen.png", alt: 'Dark-Kitchen' },
+        picture: { url: "/Dark-Kitchen.png", alt: 'Dark-Kitchen' },
         url: 'corentinmiserque.github.io/dark-kitchen/',
         github:"https://github.com/Corentinmiserque/dark-kitchen"
       },
@@ -69,7 +69,7 @@ export const useProjectsStore = defineStore({
         title: 'SunnySide',
         text: 'work in progress.',
         tag: [ 'Html', 'Scss', 'Solo'], 
-        picture: { url: "/public/SunnySide.png", alt: 'SunnySide' },
+        picture: { url: "/SunnySide.png", alt: 'SunnySide' },
         url: 'https://corentinmiserque.github.io/sunnyside-project1/',
         github:"https://github.com/Corentinmiserque/sunnyside-project1"
       },
